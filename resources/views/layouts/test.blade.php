1e7796292fb0e9717b34b36c4788cb9ea6b62ee0
Mahmoud Howa <mahmoudhowa1@gmail.com>
2024-07-25 01:00:21 +0300

    Start

---
+++ resources/views/layouts/test.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin')
+
+@section('title')
+test view title
+@endsection
+
+@section('contentheder')
+test header
+@endsection
+
+@section('contentheaderlink')   
+<a href="#">link</a>
+@endsection
+
+@section('contentheaderactive')
+this is test
+@endsection
+
+
+@section('content')
+test view
+@endsection
